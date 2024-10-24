126a66bacf906b4474e028813d404510f37b6b29
Bob Sanford <midyear66@gmail.com>
2024-10-24 14:53:33 +0000

    Initial commit of package updates
     Changes to be committed:
    	new file:   package-updates/LICENSE
    	new file:   package-updates/README.md
    	new file:   package-updates/sudoers.d/alias-chroot.docker
    	new file:   package-updates/sudoers.d/package-updates.docker
    	new file:   package-updates/sudoers.d/package-updates.openwrt
    	new file:   package-updates/zabbix_agentd.d/pkg-updates.docker.conf
    	new file:   package-updates/zabbix_agentd.d/pkg-updates.openwrt.conf
    	new file:   package-updates/zabbix_template_package_updates.xml
    	new file:   package-updates/zbx-pkg.sh

---
+++ package-updates/README.md
@@ -0,0 +1,43 @@
+# Installation
+
+## Agent setup
+
+### Docker
+
+1. sudo
+
+This template requires `sudo` to be available inside the zabbix-agent container.
+
+You can bind-mount the supplied sudoers config with:
+- `-v ./sudoers/alias-chroot.docker:/etc/sudoers.d/alias-chroot:ro`.
+- `-v ./sudoers/reboot-required.docker:/etc/sudoers.d/reboot-required:ro`.
+
+Bear in mind that this file should be owned by root and its permissions set to `0600`.
+
+2. You obviously also need to make the script available as well: `-v ./zbx-pkg.sh:/zabbix/bin/zbx-pkg.sh`.
+
+3. Pass the UserParameter config like so: `-v ./zabbix_agentd.d/pkg-updates.docker.conf:/etc/zabbix_agentd.d/pkg-updates.conf:ro`.
+
+4. To be able to chroot inside the host you need mount the rootfs like so: `-v /:/rootfs:ro`.
+
+### OpenWRT
+
+1. You need to install `sudo`:
+
+```
+opkg update && opkg install sudo
+```
+
+2. Copy `sudoers.d/package-updates.openwrt` to `/etc/sudoers.d/package-updates`.
+
+3. Copy `zbx-pkg.sh` to `/etc/zabbix_zabbix_agentd.d/bin/zbx-pkg.sh`.
+
+4. Copy `zabbix_agentd.d/reboot-required.openwrt.conf` to `/etc/zabbix_zabbix_agentd.d/reboot-required.conf`.
+
+5. Restart the agent: `/etc/init.d/zabbix_agentd restart`.
+
+## Zabbix Server setup
+
+1. Import the template `zabbix_template_package_updates.xml`.
+
+2. Apply it to your hosts
---
+++ package-updates/zbx-pkg.sh
@@ -0,0 +1,191 @@
+#!/usr/bin/env bash
+
+REPO_SYNC_INTERVAL=${REPO_SYNC_INTERVAL:-7200}
+
+usage() {
+  echo "Usage: $(basename "$0") last|list|count|sync"
+}
+
+_chroot() {
+  if [[ -e /.dockerenv ]]
+  then
+    sudo -E chroot /rootfs bash -c "$*"
+  else
+    eval "$@"
+  fi
+}
+
+arch_pkg() {
+  if _chroot command -v yay > /dev/null
+  then
+    _chroot yay "$@"
+  else
+    _chroot pacman "$@"
+  fi
+}
+
+arch_get_pkg_updates() {
+  arch_pkg -Qu 2>/dev/null
+}
+
+ubuntu_get_pkg_updates() {
+  _chroot \
+    apt list --upgradable 2>/dev/null | grep -v '^Listing...'
+}
+
+arch_repo_sync() {
+  arch_pkg -Syy >/dev/null 2>&1
+}
+
+arch_repo_sync_gentle() {
+  local last_sync now tdiff
+
+  last_sync=$(arch_get_last_repo_sync_ts)
+  now="$(date '+%s')"
+
+  tdiff=$(( now - last_sync ))
+  # echo "$now - $last_sync = $tdiff vs $REPO_SYNC_INTERVAL"
+  if [[ "$tdiff" -gt "$REPO_SYNC_INTERVAL" ]]
+  then
+    # echo "Syncing repos..." >&2
+    arch_repo_sync
+  else
+    # echo "No need to sync." >&2
+    :
+  fi
+}
+
+ubuntu_repo_sync_gentle() {
+  local cfile now tdiff last_sync
+
+  cfile="$(_ubuntu_get_cache_file)"
+  last_sync="$(ubuntu_get_last_repo_sync_ts)"
+  now="$(date '+%s')"
+
+  tdiff=$(( now - last_sync ))
+  # echo "$now - $last_sync = $tdiff vs $REPO_SYNC_INTERVAL"
+  if [[ "$tdiff" -gt "$REPO_SYNC_INTERVAL" ]]
+  then
+    ubuntu_repo_sync
+  fi
+}
+
+ubuntu_repo_sync() {
+  local cfile
+
+  cfile="$(_ubuntu_get_cache_file)"
+  _chroot apt-get update >/dev/null 2>&1
+  date '+%s' > "$cfile"
+}
+
+_ubuntu_get_cache_file() {
+  echo "/tmp/$(basename "$0")_last_sync"
+}
+
+ubuntu_get_last_repo_sync_ts() {
+  local cfile
+
+  cfile="$(_ubuntu_get_cache_file)"
+  if ! [[ -e "$cfile" ]]
+  then
+    echo -1
+    return 1
+  fi
+  cat "$cfile"
+}
+
+ubuntu_get_last_upgrade_ts() {
+  echo "Not supported yet." >&2
+}
+
+arch_pkg_updates_count() {
+  arch_get_pkg_updates | wc -l
+}
+
+ubuntu_pkg_updates_count() {
+  ubuntu_get_pkg_updates | wc -l
+}
+
+arch_get_last_upgrade_ts() {
+  pacman_log_extract_date_ts "starting full system upgrade"
+}
+
+arch_get_last_repo_sync_ts() {
+  pacman_log_extract_date_ts "synchronizing package lists"
+}
+
+pacman_log_extract_date_ts() {
+  local d
+  d=$(_chroot grep "'$*'" /var/log/pacman.log | \
+    tail -1 | sed -nr 's/^\[([^]]+)\].*/\1/p')
+  if [[ -z "$d" ]]
+  then
+    echo "Failed to determine last update time" >&2
+    return 2
+  fi
+  date -d "$d" '+%s'
+}
+
+determine_os() {
+  _chroot cat /etc/os-release | sed -nr 's/^ID=(.+)/\1/p'
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+  HOST_ID="$(determine_os)"
+
+  case "$1" in
+    help|h|--help|-h)
+      usage
+      ;;
+    count|c|--count|-c)
+      ACTION=count
+      ;;
+    last|last_update|last-update|l|lu|-l|--last|--last-update)
+      ACTION=last
+      ;;
+    last_repo_sync|lrs|-L|--last-repo-sync|--lrs)
+      ACTION=last_repo_sync
+      ;;
+    list|ls|--ls|--list|--list-available-updates)
+      ACTION=list
+      ;;
+    sync|-f|--force-sync|-s)
+      ACTION=sync
+      ;;
+    *)
+      usage
+      exit 2
+  esac
+
+  case "$HOST_ID" in
+    arch)
+      fn=arch
+      ;;
+    ubuntu|debian|raspbian|neon)
+      fn=ubuntu
+      ;;
+  esac
+
+  if [[ "$ACTION" == "sync" ]]
+  then
+    ${fn}_repo_sync
+  else
+    ${fn}_repo_sync_gentle
+  fi
+
+  case "$ACTION" in
+    count)
+      ${fn}_pkg_updates_count
+      ;;
+    last)
+      ${fn}_get_last_upgrade_ts
+      ;;
+    last_repo_sync)
+      ${fn}_get_last_repo_sync_ts
+      ;;
+    list)
+      ${fn}_get_pkg_updates
+      ;;
+  esac
+fi
